9ac37ffcecab2562e7f1b01489c4191be93461a2
Ali <lashariali@hotmail.com>
2016-03-16 19:43:24 -0400

    Completed and Tested ALU module

---
+++ ALU/testbench.tcl
@@ -0,0 +1,29 @@
+proc AddWaves {} {
+	;#Add waves we're interested in to the Wave window
+    add wave -position end sim:/testbench/sclock
+    add wave -position end sim:/testbench/sFC
+    add wave -position end sim:/testbench/sRS
+    add wave -position end sim:/testbench/sRT
+    add wave -position end sim:/testbench/sRES
+    add wave -position end sim:/testbench/sZERO
+    add wave -position end sim:/testbench/st
+   
+}
+
+vlib work
+
+;# Compile components if any
+vcom ALU.vhd
+vcom testbench.vhd
+
+;# Start simulation
+vsim testbench
+
+;# Generate a clock with 1ns period
+force -deposit sclock 0 0 ns, 1 0.5 ns -repeat 1 ns
+
+;# Add the waves
+AddWaves
+
+;# Run for 50 ns
+run 50ns
